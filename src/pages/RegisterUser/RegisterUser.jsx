929810f9da1c7ce9d47ba86d274c2467d20b9747
Akndju <davidd.akindoju@gmail.com>
2021-04-23 08:28:35 +0100

    Corrected update page

--- src/pages/RegisterUser/RegisterUser.jsx
+++ src/pages/RegisterUser/RegisterUser.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './RegisterUser.css';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
--- src/pages/UpdateProfile/UpdateProfile.jsx
+++ src/pages/UpdateProfile/UpdateProfile.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import './UpdateProfile.css';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
-import { useHistory } from 'react-router';
 
 const UpdateProfile = () => {
-  const history = useHistory();
-
   const validateSchema = Yup.object().shape({
-    name: Yup.string()
-      .min(2, 'Name should be at least 2 characters')
-      .required('Name is required'),
+    currentName: Yup.string()
+      .min(2, 'Current Name should be at least 2 characters')
+      .required('Current Name is required'),
 
-    email: Yup.string().email('Invalid Email').required('Email is required'),
+    newName: Yup.string()
+      .min(2, 'New Name should be at least 2 characters')
+      .required('New Name is required'),
+
+    currentEmail: Yup.string()
+      .email('Invalid Email')
+      .required('Current Email is required'),
+
+    newEmail: Yup.string()
+      .email('Invalid Email')
+      .required('New Email is required'),
   });
 
   return (
@@ -20,29 +26,34 @@ const UpdateProfile = () => {
       <h2>Update Profile</h2>
       <Formik
         initialValues={{
-          name: '',
-          email: '',
+          currentName: '',
+          newName: '',
+          currentEmail: '',
+          newEmail: '',
         }}
         validationSchema={validateSchema}
         onSubmit={async (values, { setSubmitting, resetForm }) => {
           setSubmitting(true);
 
           await fetch('http://localhost:5000/update', {
-            method: 'post',
+            method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-              name: values.name,
-              email: values.email,
+              currentName: values.currentName,
+              newName: values.newName,
+              currentEmail: values.currentEmail,
+              newEmail: values.newEmail,
             }),
-          });
-          // .then((res) => res.json())
-          // .then((data) => {
-          //   if (data === 'Successful') {
-          //     history.push('/homepage');
-          //   } else {
-          //     alert('Something went wrong');
-          //   }
-          // });
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              if (data === 'Successful') {
+                alert('Successful');
+                resetForm();
+              } else {
+                alert('Something went wrong');
+              }
+            });
 
           setSubmitting(false);
         }}
@@ -61,30 +72,64 @@ const UpdateProfile = () => {
               <div className="credentials">
                 <input
                   type="name"
-                  name="name"
+                  name="currentName"
                   onChange={handleChange}
                   onBlur={handleBlur}
-                  value={values.name}
+                  value={values.currentName}
                   required
                 />
-                <label htmlFor="name">Name</label>
+                <label htmlFor="currentName">Current Name</label>
                 <div className="errMsg-span">
-                  {errors.name && touched.name ? errors.name : null}
+                  {errors.currentName && touched.currentName
+                    ? errors.currentName
+                    : null}
+                </div>
+              </div>
+
+              <div className="credentials">
+                <input
+                  type="name"
+                  name="newName"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.newName}
+                  required
+                />
+                <label htmlFor="newName">New Name</label>
+                <div className="errMsg-span">
+                  {errors.newName && touched.newName ? errors.newName : null}
                 </div>
               </div>
 
               <div className="credentials">
                 <input
                   type="email"
-                  name="email"
+                  name="currentEmail"
                   onChange={handleChange}
                   onBlur={handleBlur}
-                  value={values.email}
+                  value={values.currentEmail}
                   required
                 />
-                <label htmlFor="email">Email</label>
+                <label htmlFor="currentEmail">Current Email</label>
                 <div className="errMsg-span">
-                  {errors.email && touched.email ? errors.email : null}
+                  {errors.currentEmail && touched.currentEmail
+                    ? errors.currentEmail
+                    : null}
+                </div>
+              </div>
+
+              <div className="credentials">
+                <input
+                  type="email"
+                  name="newEmail"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.newEmail}
+                  required
+                />
+                <label htmlFor="newEmail">New Email</label>
+                <div className="errMsg-span">
+                  {errors.newEmail && touched.newEmail ? errors.newEmail : null}
                 </div>
               </div>
 
